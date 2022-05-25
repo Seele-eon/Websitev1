c7ef8b30a06210f78f2c91109243793a2f68cb20
Seele-eon <99353828+Seele-eon@users.noreply.github.com>
2022-05-25 13:50:25 -0400

    Some refactoring

--- js/whatfor.js
+++ js/whatfor.js
@@ -6,6 +6,10 @@ var popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
 const textList = [" You gotta close the tab", "For real...", "I can't close the tab for you."];
 const outButton = document.querySelector('.btn-outline-secondary');
 const textToChange = document.querySelector('#textToChange');
+const whatforgif1 = document.querySelector('#whatforgif1');
+const whatforimg1 = document.querySelector('#whatforimg1');
+const whatforgif2 = document.querySelector('#whatforgif2');
+const whatforimg2 = document.querySelector('#whatforimg2');
 const whatforgif3 = document.querySelector('#whatforgif3');
 const whatforimg3 = document.querySelector('#whatforimg3');
 outButton.addEventListener('click', function handleClick(){
@@ -29,14 +33,21 @@ function isScrolledIntoView(el) {
     return isVisible;
 }
 
-document.body.addEventListener("scroll", function handleScroll(e){
+function initiateGif(el1, el2){
+    el1.classList.add("hidden");
+    el2.classList.remove("hidden");
+}
+
+document.body.addEventListener("scroll", function handleScroll(){
+    if(isScrolledIntoView(whatforimg1) && whatforgif1.classList.contains("hidden")){
+      initiateGif(whatforimg1, whatforgif1);
+    }
+
     if(isScrolledIntoView(whatforimg2) && whatforgif2.classList.contains("hidden")){
-      whatforimg2.classList.add("hidden");
-      whatforgif2.classList.remove("hidden");
+      initiateGif(whatforimg2, whatforgif2);
     }
 
     if(isScrolledIntoView(whatforimg3) && whatforgif3.classList.contains("hidden")){
-      whatforimg3.classList.add("hidden");
-      whatforgif3.classList.remove("hidden");
+      initiateGif(whatforimg3, whatforgif3);
     }
 });

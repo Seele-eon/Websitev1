e80d63a218c6a93beaf83d32d49b36297907fa97
Seele-eon <99353828+Seele-eon@users.noreply.github.com>
2022-05-25 13:16:23 -0400

    lazy load for key gifs

--- js/whatfor.js
+++ js/whatfor.js
@@ -6,7 +6,8 @@ var popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
 const textList = [" You gotta close the tab", "For real...", "I can't close the tab for you."];
 const outButton = document.querySelector('.btn-outline-secondary');
 const textToChange = document.querySelector('#textToChange');
-
+const whatforgif3 = document.querySelector('#whatforgif3');
+const whatforimg3 = document.querySelector('#whatforimg3');
 outButton.addEventListener('click', function handleClick(){
 
   for (let i = 0; i < textList.length; i++) {
@@ -15,3 +16,27 @@ outButton.addEventListener('click', function handleClick(){
   }, 1270*i);
 }
 })
+
+function isScrolledIntoView(el) {
+    var rect = el.getBoundingClientRect();
+    var elemTop = rect.top;
+    var elemBottom = rect.bottom;
+
+    // Only completely visible elements return true:
+    var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+    // Partially visible elements return true:
+    //isVisible = elemTop < window.innerHeight && elemBottom >= 0;
+    return isVisible;
+}
+
+document.body.addEventListener("scroll", function handleScroll(e){
+    if(isScrolledIntoView(whatforimg2) && whatforgif2.classList.contains("hidden")){
+      whatforimg2.classList.add("hidden");
+      whatforgif2.classList.remove("hidden");
+    }
+
+    if(isScrolledIntoView(whatforimg3) && whatforgif3.classList.contains("hidden")){
+      whatforimg3.classList.add("hidden");
+      whatforgif3.classList.remove("hidden");
+    }
+});

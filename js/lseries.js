1ed8f84df7d3412cd0aef55112d935486657d3ec
Seele-eon <99353828+Seele-eon@users.noreply.github.com>
2022-05-27 13:15:44 -0400

    minified

--- js/lseries.js
+++ js/lseries.js
@@ -1,10 +1,1 @@
-const textList = ['2', '3', '4', '.', '..', '...', 'F', 'Final','Final-1'];
-
-const cycle = document.querySelector("#cycle");
-let i = 0;
-const cycleText = () => {
-  cycle.innerHTML = textList[i];
-  i = ++i % textList.length;
-};
-cycleText();
-setInterval(cycleText, 1100);
+const textList=["2","3","4",".","..","...","F","Final","Final-1"],cycle=document.querySelector("#cycle");let i=0;const cycleText=()=>{cycle.innerHTML=textList[i],i=++i%textList.length};cycleText(),setInterval(cycleText,1100);
